331b046fb41c4e8a3d5828ae0e50cee98ff98bc2
Crowdbotics <team@crowdbotics.com>
2020-10-25 01:09:01 +0000

    remove package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -2,7 +2,6 @@ export const installed_blueprints = [
   //@BlueprintInsertion
 { name: 'EmailAuth1160957', human_name: 'Email Auth 1', access_route: 'EmailAuth1160957', icon: 'envelope-o'},
 { name: 'Tutorial2160956', human_name: 'Tutorial 2', access_route: 'Tutorial2160956', icon: 'question-circle'},
-{ name: 'UserProfile4160954', human_name: 'User Profile 4', access_route: 'UserProfile4160954'},
 { name: 'Settings6160953', human_name: 'Settings 6', access_route: 'Settings6160953'},
 { name: 'NotificationList7160952', human_name: 'Notification List 7', access_route: 'NotificationList7160952'},
 { name: 'BlankScreen0160813', human_name: 'Blank Screen 0', access_route: 'BlankScreen0160813'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -7,7 +7,6 @@ import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import EmailAuth1160957Navigator from '../features/EmailAuth1160957/navigator';
 import Tutorial2160956Navigator from '../features/Tutorial2160956/navigator';
-import UserProfile4160954Navigator from '../features/UserProfile4160954/navigator';
 import Settings6160953Navigator from '../features/Settings6160953/navigator';
 import NotificationList7160952Navigator from '../features/NotificationList7160952/navigator';
 import BlankScreen0160813Navigator from '../features/BlankScreen0160813/navigator';
@@ -22,7 +21,6 @@ const AppNavigator = {
     //@BlueprintNavigationInsertion
 EmailAuth1160957: { screen: EmailAuth1160957Navigator },
 Tutorial2160956: { screen: Tutorial2160956Navigator },
-UserProfile4160954: { screen: UserProfile4160954Navigator },
 Settings6160953: { screen: Settings6160953Navigator },
 NotificationList7160952: { screen: NotificationList7160952Navigator },
 BlankScreen0160813: { screen: BlankScreen0160813Navigator },
