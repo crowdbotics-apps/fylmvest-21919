eb186bc749d8924669845cf149ba1bb80d79418a
Crowdbotics <team@crowdbotics.com>
2020-10-25 01:09:04 +0000

    remove package blueprint-react-native-settings-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -7,7 +7,6 @@ import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import EmailAuth1160957Navigator from '../features/EmailAuth1160957/navigator';
 import Tutorial2160956Navigator from '../features/Tutorial2160956/navigator';
-import Settings6160953Navigator from '../features/Settings6160953/navigator';
 import NotificationList7160952Navigator from '../features/NotificationList7160952/navigator';
 import BlankScreen0160813Navigator from '../features/BlankScreen0160813/navigator';
 import SignIn41160665Navigator from '../features/SignIn41160665/navigator';
@@ -21,7 +20,6 @@ const AppNavigator = {
     //@BlueprintNavigationInsertion
 EmailAuth1160957: { screen: EmailAuth1160957Navigator },
 Tutorial2160956: { screen: Tutorial2160956Navigator },
-Settings6160953: { screen: Settings6160953Navigator },
 NotificationList7160952: { screen: NotificationList7160952Navigator },
 BlankScreen0160813: { screen: BlankScreen0160813Navigator },
 SignIn41160665: { screen: SignIn41160665Navigator },
